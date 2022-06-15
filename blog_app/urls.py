7d0ff540873dd55abd0caf442357dd4b028cb183
python <fooehan3@gmail.com>
2022-06-16 00:14:58 +0430

    updeta

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-
+app_name = "blog"
 urlpatterns = [
-    path("<int:pk>", views.post_datal)
+    path("<int:pk>", views.post_datal, name="detail")
 ]
